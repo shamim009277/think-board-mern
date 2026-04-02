302701890c981f0e5fa44b78d8660b8780fb287b
Towhedul <towhidshamim009@gmail.com>
2026-04-02 09:40:38 +0600

    basic rest api

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!. This is the backend server. You can add your API routes here.');
-})
 
+app.get('/api/motes/', (req, res) => {
+  res.status(200).json({ message: 'get all motes' });
+});
+
+app.post('/api/motes/', (req, res) => {
+  res.status(201).json({ message: 'create a new mote successfully' });
+});
+
+app.get('/api/motes/:id', (req, res) => {
+  res.status(200).json({ message: 'get a specific mote' });
+});
+
+app.put('/api/motes/:id', (req, res) => {
+    res.status(200).json({ message: 'update a specific mote successfully' });
+});
+
+app.delete('/api/motes/:id', (req, res) => {
+    res.status(200).json({ message: 'delete a specific mote successfully' });
+});
 
 
 app.listen(5001, () => {
