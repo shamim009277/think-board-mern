6e89e7e5a06853ce72ac66abf43bdd1f399fc564
Towhedul <towhidshamim009@gmail.com>
2026-04-02 15:19:35 +0600

    data store by model

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,16 @@
 import noteRouter from './routes/note.route.js';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
+import express from 'express';
 
 dotenv.config();
-
-import express from 'express';
 const app = express();
+
+app.use(express.json());
 
 app.use('/api/notes', noteRouter);
 
 connectDB();
-
-app.use(express.json());
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
--- backend/src/controllers/note.controller.js
+++ backend/src/controllers/note.controller.js
@@ -1,19 +1,77 @@
-export const getAllNotes = (req, res) => {
-  res.status(200).json({ message: 'get all notes' });
+import Note from '../models/noteModel.js';
+
+export const getAllNotes = async (req, res) => {
+    try {
+        const notes = await Note.find().sort({ createdAt: -1 });
+        
+        if (!notes || notes.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "No notes found",
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            count: notes.length,
+            data: notes,
+        });
+
+    } catch (error) {
+        console.error("Get Notes Error:", error.message);
+        return res.status(500).json({
+            success: false,
+            message: "Server Error",
+            error: error.message,
+        });
+    }
 };
 
-export const getNoteById = (req, res) => {
-    res.status(200).json({ message: 'get a specific note' });
+export const getNoteById = async (req, res) => {
+    try {
+        const note = await Note.findById(req.params.id);
+
+        if (!note) {
+            return res.status(404).json({ status: false, message: 'Note not found' });
+        }
+
+        res.status(200).json({ status: true, message: 'get note by id successfully', data: note });
+    } catch (error) {
+        res.status(500).json({ status: false, message: error.message });
+    }
 };
 
-export const createNote = (req, res) => {
-    res.status(201).json({ message: 'create a new note successfully' });
+export const createNote = async (req, res) => {
+    try {
+        const note = await Note.create(req.body);
+        res.status(201).json({ status: true, message: 'create a new note successfully', data: note });
+    } catch (error) {
+        res.status(500).json({ status: false, message: error.message });
+    }
 };
 
-export const updateNote = (req, res) => {
-    res.status(200).json({ message: 'update a specific note successfully' });
+export const updateNote = async (req, res) => {
+    try {
+        const note = await Note.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        if (!note) {
+            return res.status(404).json({ status: false, message: 'Note not found' });
+        }
+
+        res.status(200).json({ status: true, message: 'update a specific note successfully', data: note });
+    } catch (error) {
+        res.status(500).json({ status: false, message: error.message });
+    }
 };
 
-export const deleteNote = (req, res) => {
-    res.status(200).json({ message: 'delete a specific note successfully' });
+export const deleteNote = async (req, res) => {
+    try {
+        const note = await Note.findByIdAndDelete(req.params.id);
+
+        if (!note) {
+            return res.status(404).json({ status: false, message: 'Note not found' });
+        }
+        res.status(200).json({ status: true, message: 'delete a specific note successfully', data: note });
+    } catch (error) {
+        res.status(500).json({ status: false, message: error.message });
+    }
 };
