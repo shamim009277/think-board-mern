028f28e284bc074ed36e2f54c9cbe9320c7ec3dd
Towhedul <towhidshamim009@gmail.com>
2026-04-02 16:13:13 +0600

    react router and toast notification

---
+++ frontend/src/assets/CreatePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CreatePage = () => {
+  return (
+    <div>CreatePage</div>
+  )
+}
+
+export default CreatePage
---
+++ frontend/src/assets/DeatilsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const DeatilsPage = () => {
+  return (
+    <div>DeatilsPage</div>
+  )
+}
+    
+export default DeatilsPage
